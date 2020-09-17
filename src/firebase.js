300249fcb71e3b6db1a80a8f40e78b0aa883d0f1
jack <actionjacks22@gmail.com>
2020-09-17 20:27:55 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAHIN0d0kGNVRWlaJpqngWu-dEVJCqkuGg",
+  authDomain: "api-battlenet-app.firebaseapp.com",
+  databaseURL: "https://api-battlenet-app.firebaseio.com",
+  projectId: "api-battlenet-app",
+  storageBucket: "api-battlenet-app.appspot.com",
+  messagingSenderId: "545406389009",
+  appId: "1:545406389009:web:e83c5b349789cb24d39ab0",
+  measurementId: "G-RQ7G9LVQP0",
+};
