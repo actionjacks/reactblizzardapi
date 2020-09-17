80fb44af4348d8e8374f7a20d147dd37b6f5369a
jack <actionjacks22@gmail.com>
2020-09-17 21:53:03 +0200

    first test for request

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  //test api request
+  fetch(
+    "https://us.api.blizzard.com/hearthstone/cards?locale=en_US&class=shaman&manaCost=1&access_token=USPxgDBa48WEeftdq1vdMJ5SmhQh3EGsPC"
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
   return (
     <div className="app">
       <h1>hello web world</h1>
