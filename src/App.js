6928df2c7f09f4942547ec8eb515bc481612b1a7
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-03-19 15:24:07 +0100

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+import Home from "./components/Home";
 import Heartstone from "./components/Heartstone";
+import Diablo from "./components/Diablo";
+import Starcraft from "./components/Starcraft";
+import Wow from "./components/Wow";
 
 import "./App.css";
+
 function App() {
   return (
-    <div className="app">
-      <p>ROUTES GO THERE</p>
-      <Heartstone />
-    </div>
+    <Router>
+      <div>
+        <p>Select Game</p>
+        <ul>
+          <li>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/heartstone">heartstone</NavLink>
+          </li>
+          <li>
+            <NavLink to="/diablo">diablo</NavLink>
+          </li>
+          <li>
+            <NavLink to="/starcraft">starcraft</NavLink>
+          </li>
+          <li>
+            <NavLink to="/wow">wow</NavLink>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/wow">
+            <Wow />
+          </Route>
+          <Route path="/starcraft">
+            <Starcraft />
+          </Route>
+          <Route path="/diablo">
+            <Diablo />
+          </Route>
+          <Route path="/heartstone">
+            <Heartstone />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Wow/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Wow() {
+  return (
+    <div>
+      <h3>Wow</h3>
+    </div>
+  );
+}
+
+export default Wow;
